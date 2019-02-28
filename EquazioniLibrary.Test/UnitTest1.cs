124aaaa425d0cfe98fdbcf2ff8c63f722941916b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-02-28 09:01:35 +0100

    Vediamo se è determinata

--- EquazioniLibrary.Test/UnitTest1.cs
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -7,9 +7,26 @@ namespace EquazioniLibrary.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestisDetermined()
         {
-
+            double n1 = 1;bool risultato = true;//faccio le variabili
+            bool risultato2 = EquazioniLibrary.Equazioni.IsDetermined(n1);//lo va
+            Assert.AreEqual(risultato, risultato2);
         }
+        [TestMethod]
+        public void TestisDetermined2()
+        {
+            double n1 = 0; bool risultato = false;//faccio le variabili
+            bool risultato2 = EquazioniLibrary.Equazioni.IsDetermined(n1);//lo va
+            Assert.AreEqual(risultato, risultato2);
+        }
+        [TestMethod]
+        public void TestisDetermined3()
+        {
+            double n1 = -1; bool risultato = true;//faccio le variabili
+            bool risultato2 = EquazioniLibrary.Equazioni.IsDetermined(n1);//lo va
+            Assert.AreEqual(risultato, risultato2);
+        }
+        
     }
 }
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -8,6 +8,27 @@ namespace EquazioniLibrary
 {
     public class Equazioni
     {
+        public static bool IsDetermined(double num1)
+        {
+            bool uscita = false;
 
+            if (num1 != 0)
+            {
+                uscita = true;
+            }
+
+            return uscita;
+        }
+        public static bool IsInconsisted(double num1)
+        {
+            bool ritorno = false;
+
+            if (num1 == 0)
+            {
+                ritorno = true;
+            }
+
+            return ritorno;
+        }
     }
 }
