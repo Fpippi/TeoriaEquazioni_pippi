2a1ca898d7e6ba9bb08dcbd926c8b919f33c5066
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-02-28 09:05:28 +0100

    Verifichiamo se è impossibile

--- EquazioniLibrary.Test/UnitTest1.cs
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -10,23 +10,44 @@ namespace EquazioniLibrary.Test
         public void TestisDetermined()
         {
             double n1 = 1;bool risultato = true;//faccio le variabili
-            bool risultato2 = EquazioniLibrary.Equazioni.IsDetermined(n1);//lo va
-            Assert.AreEqual(risultato, risultato2);
+            bool risultato2 = EquazioniLibrary.Equazioni.IsDetermined(n1);//lo vado a richiamare
+            Assert.AreEqual(risultato, risultato2);//lo confronto
         }
         [TestMethod]
         public void TestisDetermined2()
         {
-            double n1 = 0; bool risultato = false;//faccio le variabili
-            bool risultato2 = EquazioniLibrary.Equazioni.IsDetermined(n1);//lo va
+            double n1 = 0; bool risultato = false;
+            bool risultato2 = EquazioniLibrary.Equazioni.IsDetermined(n1);
             Assert.AreEqual(risultato, risultato2);
         }
         [TestMethod]
         public void TestisDetermined3()
         {
-            double n1 = -1; bool risultato = true;//faccio le variabili
-            bool risultato2 = EquazioniLibrary.Equazioni.IsDetermined(n1);//lo va
+            double n1 = -1; bool risultato = true;
+            bool risultato2 = EquazioniLibrary.Equazioni.IsDetermined(n1);
             Assert.AreEqual(risultato, risultato2);
         }
-        
+        [TestMethod]
+        public void TestIsconsisted()
+        {
+            double n1 = 0; bool risultato = true;
+            bool risultato2 = EquazioniLibrary.Equazioni.IsInconsisted(n1);
+            Assert.AreEqual(risultato, risultato2);
+        }
+        [TestMethod]
+        public void TestIsconsisted2()
+        {
+            double n1 = 1; bool risultato = false;
+            bool risultato2 = EquazioniLibrary.Equazioni.IsInconsisted(n1);
+            Assert.AreEqual(risultato, risultato2);
+        }
+        [TestMethod]
+        public void TestIsconsisted3()
+        {
+            double n1 = -1; bool risultato = false;
+            bool risultato2 = EquazioniLibrary.Equazioni.IsInconsisted(n1);
+            Assert.AreEqual(risultato, risultato2);
+        }
+
     }
 }
